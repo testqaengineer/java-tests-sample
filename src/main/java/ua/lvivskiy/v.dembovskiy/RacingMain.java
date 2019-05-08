8836908cd37b7f4f89dab70687ed928c031b19bb
pgrk <testqaengineerautomation@gmail.com>
2019-05-08 12:54:31 +0300

    nothing

--- src/main/java/ua/lvivskiy/v.dembovskiy/RacingMain.java
+++ src/main/java/ua/lvivskiy/v.dembovskiy/RacingMain.java
@@ -4,19 +4,19 @@ public class RacingMain {
 
     public static void main(String... args) {
 
-        Car car1 = new Car("Mersedes", "Black",30, 50000,30, 80, true , 10);
+        main.java.ua.lvivskiy.v.dembovskiy.Car car1 = new main.java.ua.lvivskiy.v.dembovskiy.Car("Mersedes", "Black",30, 50000,30, 80, true , 10);
         car1.refuel( 5);
         car1.turnOn();
         car1.ride( 20);
         car1.turnOff();
 
-        Car car2 = new Car("BMW", "Wight", 60, 35,false, 7);
+        main.java.ua.lvivskiy.v.dembovskiy.Car car2 = new main.java.ua.lvivskiy.v.dembovskiy.Car("BMW", "Wight", 60, 35,false, 7);
         car2.refuel( 8);
         car2.turnOn();
         car2.ride( 67);
         car2.turnOff();
 
-        Car car3 = new Car("Maserati", "Blue",90, 9000, 90, 90, true , 8);
+        main.java.ua.lvivskiy.v.dembovskiy.Car car3 = new main.java.ua.lvivskiy.v.dembovskiy.Car("Maserati", "Blue",90, 9000, 90, 90, true , 8);
         car3.refuel( 20);
         car3.turnOn();
         car3.ride( 80);
